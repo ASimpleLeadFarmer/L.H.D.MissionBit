6d43b159cc57ef7419a1ce8910a94d8a95f21816
ASimpleLeadFarmer <lhenry.dvii@gmail.com>
2016-02-25 17:54:53 -0800

    started js game

---
+++ Game Dev/state0.js
@@ -0,0 +1,17 @@
+var demo = {};
+demo.state = function(){};
+
+
+demo.state0.prototype = {
+    preload: function(){
+        game.load.image('trees', 'assets/background/treeBG.pmg');
+        game.load.spreitesheet('adam', 'assets/spritesheets/adamSpritesheet.png');
+    },
+    create: function(){
+    
+    },
+    update: function(){
+    
+    },
+
+};
