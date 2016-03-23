8dcfc69d30e04541434d6c62d69ab6f2b4bc5f6c
20juanl <20juanl@gatewaymiddle.net>
2016-03-22 17:17:37 -0700

    Update state0.js

--- ZAMBI_SLYR_2016_420WEED/state0.js
+++ ZAMBI_SLYR_2016_420WEED/state0.js
@@ -1,13 +1,13 @@
 var demo = {};
 var speed = 6;
-var adam;
+var Ford;
 demo.state0 = function(){};
 
 
 demo.state0.prototype = {
 	preload: function(){
 		game.load.image('trees', 'assets/background/treeBG.png');
-		game.load.spritesheet('adam', 'assets/spritesheets/adamSpritesheet.png', 240, 370);
+		game.load.spritesheet('Ford', 'assets/spritesheets/adamSpritesheet.png', 240, 370);
         
 	},
 
@@ -16,40 +16,40 @@ demo.state0.prototype = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         var trees = game.add.sprite(0, 0, 'trees');
-        adam = game.add.sprite(0, 450, 'adam');
-        adam.animations.add('walk', [0, 1, 2, 3, 4]);
-        adam.anchor.setTo(0.5, 0.5);
-        adam.scale.setTo(0.7, 0.7);
-        game.physics.enable(adam);
+        Ford = game.add.sprite(0, 450, 'Ford');
+        Ford.animations.add('walk', [0, 1, 2, 3, 4]);
+        Ford.anchor.setTo(0.5, 0.5);
+        Ford.scale.setTo(0.7, 0.7);
+        game.physics.enable(Ford);
         adam.body.collideWorldBounds = true;
-        game.camera.follow(adam);
+        game.camera.follow(Ford);
         game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
         
 	},
 
 	update: function(){
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-             adam.x = adam.x + speed;
-             adam.animations.play('walk', 20, true);
-            adam.scale.setTo(0.7, 0.7);
+             Ford.x = Ford.x + speed;
+             Ford.animations.play('walk', 20, true);
+            Ford.scale.setTo(0.7, 0.7);
           }
        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-            adam.x = adam.x - speed;
-            adam.animations.play('walk', 20, true);
-           adam.scale.setTo(-0.7, 0.7);
+            Ford.x = adam.x - speed;
+            Ford.animations.play('walk', 20, true);
+           Ford.scale.setTo(-0.7, 0.7);
        }
         else{
-            adam.animations.stop('walk');
+            Ford.animations.stop('walk');
         }
        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-           adam.y = adam.y - speed;
+           Ford.y = adam.y - speed;
            
-           if(adam.y < 400){
-               adam.y = 400;
+           if(Ford.y < 400){
+               Ford.y = 400;
            }
         }
        if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-             adam.y = adam.y + speed;
+             Ford.y = Ford.y + speed;
         }
     }
 };
