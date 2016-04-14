4f94fe092c54705ae4ab5c174284d4b72296caf8
ibbecknell <missionbit@Missions-MacBook-Air.local>
2016-04-14 11:27:39 -0700

    adding my version

--- GameDev/state0.js
+++ GameDev/state0.js
@@ -1,31 +1,37 @@
 var demo = {};
-var speed = 6;
+var speed = 2;
 var adam;
+var cursors;
 demo.state0 = function(){};
 
 
 demo.state0.prototype = {
 	preload: function(){
 		game.load.image('trees', 'assets/background/wasteland3.png');
-		game.load.spritesheet('adam', 'assets/spritesheets/ford.png', 255, 241);
+		game.load.spritesheet('adam', 'assets/spritesheets/tank.png', 199, 82);
         
 	},
 
 	create: function(){
-        game.world.setBounds(0,0, 2813, 1000);
+        game.world.setBounds(0,0, 4000, 800);
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         var trees = game.add.sprite(0, 0, 'trees');
-        adam = game.add.sprite(0, 450, 'adam');
-        adam.animations.add('walk', [0, 1, 2, 3]);
-        adam.anchor.setTo(0.5, 0.5);
-        adam.scale.setTo(0.7, 0.7);
+        adam = game.add.sprite(120, 550, 'adam');
+//        adam.animations.add('walk', [0, 1, 2]);
+//        adam.anchor.setTo(0.5, 0.5);
+        adam.scale.setTo(2, 2);
         game.physics.enable(adam);
         adam.body.collideWorldBounds = true;
         game.camera.follow(adam);
-        game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
-        trees.height = game.height = 1000;
-        trees.width = game.width = 2813;
+//        game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
+        trees.height = 800;
+        trees.width = 4000;
+        
+            //  The score
+    scoreText = game.add.text(650, 16, 'score: 0', { fontSize: '25px', fill: '	#FFFFFF' });
+        cursors = game.input.keyboard.createCursorKeys();
+        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
 	},
 
@@ -33,27 +39,28 @@ demo.state0.prototype = {
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
              adam.x = adam.x + speed;
              adam.animations.play('walk', 20, true);
-            adam.scale.setTo(0.7, 0.7);
+            adam.scale.setTo(2, 2);
           }
        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
             adam.x = adam.x - speed;
             adam.animations.play('walk', 20, true);
-           adam.scale.setTo(-0.7, 0.7);
+           adam.scale.setTo(2,2);
        }
         else{
             adam.animations.stop('walk');
         }
-//       if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-//           adam.y = adam.y - speed;
-//           
-//           if(adam.y < 400){
-//               adam.y = 400;
-//           }
-//        }
+       if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+           adam.y = adam.y - speed;
+           
+           if(adam.y < 550){
+               adam.y = 550;
+           }
+        }
        if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
              adam.y = adam.y + speed;
         }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)&& adam.y = 1000){
-            adam.body.velocity.y = -350
+//        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)&& adam.y = 1000){
+//            adam.body.velocity.y = -350
+//        }
     }
 };
